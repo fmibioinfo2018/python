6b90854d5a58b4e1eb0f2d9170e1ff10fba5d3fc
ALambova <sanilambova@gmail.com>
2019-03-12 01:46:21 +0200

    neo4j_test.py update

--- Aleksandrina/neo4j_test.py
+++ Aleksandrina/neo4j_test.py
@@ -1,8 +1,7 @@
 
 from neo4j import GraphDatabase, basic_auth
+import re
 
-#  "bolt://34.224.2.79:34310", 
-#     auth=basic_auth("neo4j", "ingredient-vibrations-files"))
 class MutationsDB:
     def __init__(self, uri, auth):
         self.driver = GraphDatabase.driver(uri, auth=auth)
@@ -36,14 +35,15 @@ class MutationsDB:
                 query = "match (a:{}), (b:{}) where a.name = '{}' and b.name = '{}' create (a)-[r:{}]->(b) ".format(from_node, to_node, first, second, type)
                 for record in records[1:len(records)-1]:
                     r = record.split(':')
-                    if r[0] == "000": continue
+                    if r[0][0].isdigit():
+                        r[0] = 'x' + r[0]
                     if len(r) < 2:
                         r.append(' ')
                     query += "set r.{} = '{}' ".format(r[0], r[1])
                 query += "return r"
                 count += 1
                 session.run(query)
-                #print(query)
+                print(query)
         #print(count)
         f.close()
 
@@ -53,7 +53,9 @@ class MutationsDB:
 
 
 if __name__ == "__main__":
-    db = MutationsDB("bolt://34.224.2.79:34310", basic_auth("neo4j", "ingredient-vibrations-files"))
-    db.add_nodes_from_file("../nosql/neo4j/test_data/Sample.csv", "Sample")
-    db.add_nodes_from_file("../nosql/neo4j/test_data/HugoSymbol.csv", "HugoSymbol")
+    db = MutationsDB("bolt://34.207.155.255:39042", basic_auth("neo4j", "appropriation-finishes-feeling"))
+    #db.add_nodes_from_file("../nosql/neo4j/test_data/Sample.csv", "Sample")
+    #db.add_nodes_from_file("../nosql/neo4j/test_data/HugoSymbol.csv", "HugoSymbol")
     db.add_relationships_from_file("../nosql/neo4j/test_data/mutation.csv", "mutation", "Sample", "HugoSymbol")
+    db.close()
+    
